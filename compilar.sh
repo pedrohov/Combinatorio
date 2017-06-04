c3d01cad3b66fa62eabe3076c3485f54b59f0f46
FellipePeixoto <rochapeduardo@gmail.com>
2017-06-04 18:30:56 -0300

    primeira mesclagem de codigo paralelo: mesclagem feita com commit anterior

--- compilar.sh
+++ compilar.sh
@@ -1,2 +1,9 @@
 find -name "*.c" > names.txt
-gcc @names.txt 2> log.txt -o saida
+msg=""
+msg=$(gcc @names.txt 2>&1 -lm -o saida)
+
+if [ "$msg" != "" ];
+then
+ echo $msg> log.txt
+ gedit log.txt
+fi
--- main.c
+++ main.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "matrix.h"
-#include "tests.h"
 
 int main(int argc, char const *argv[])
 {
   /*ponteiro para os arquivos de entrada*/
-  // FILE *datasetTrainning = fopen(argv[1],"r");
-  // FILE *datasetComparison = fopen(argv[2],"r");
-
-  MATRIX A = (MATRIX) MATaloca(1,2);
-  MATRIX B = (MATRIX) MATaloca(2,1);
-
-  test_GenerateRandomValues(A,2,20);
-  test_GenerateRandomValues(B,50,200);
-
-  MATRIX result = (MATRIX) MATprodutoMatricial(A,B);
-
-  test_WriteMatrixIn_R_format(A,"Amatriz.r");
-  test_WriteMatrixIn_R_format(B,"Bmatriz.r");
-
-  printf("%d\n",MATprodutoEscalar(A,B));
+  FILE *datasetTrainning = fopen(argv[1],"r");
+  FILE *datasetComparison = fopen(argv[2],"r");
 
   /*liberacao dos arquivos de entrada*/
-  // fclose(datasetTrainning);
-  // fclose(datasetComparison);
+  fclose(datasetTrainning);
+  fclose(datasetComparison);
 
   return 0;
 }
